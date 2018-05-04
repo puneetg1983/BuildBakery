2be88e184dae7433d1e4aa3c6b90a0207b58e560
Puneet Gupta <puneetg@microsoft.com>
2018-05-04 15:56:57 -0700

    First Commit

---
+++ BuildBakery/Controllers/HomeController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using BuildBakery;
+using BuildBakery;
+
+namespace BuildBakery.Controllers
+{
+    public class HomeController : Controller
+    {
+        private buggybakerydbEntities db = new buggybakerydbEntities();
+
+        // GET: NewProducts
+        public ActionResult Index()
+        {
+            return View(db.NewProducts.ToList());
+        }
+
+        // GET: NewProducts/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NewProduct newProduct = db.NewProducts.Find(id);
+            if (newProduct == null)
+            {
+                return HttpNotFound();
+            }
+
+            switch(id)
+            {
+                case 1:
+                    Utility.MakeDatabaseCallAndFetchData();
+                    break;
+                case 2:
+                    await Utility.CallWebApi();
+                    break;
+                case 3:
+                    Utility.MakeOutboundConnections();
+                    break;
+                case 4:
+                    Utility.SlowSqlCall();
+                    break;
+                case 6:
+                    Utility.ThrowException();
+                    break;
+
+
+            }
+            return View(newProduct);
+        }
+
+        // GET: NewProducts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: NewProducts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Description,Price,ImageName")] NewProduct newProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                db.NewProducts.Add(newProduct);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(newProduct);
+        }
+
+        // GET: NewProducts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NewProduct newProduct = db.NewProducts.Find(id);
+            if (newProduct == null)
+            {
+                return HttpNotFound();
+            }
+            return View(newProduct);
+        }
+
+        // POST: NewProducts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Description,Price,ImageName")] NewProduct newProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(newProduct).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(newProduct);
+        }
+
+        // GET: NewProducts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NewProduct newProduct = db.NewProducts.Find(id);
+            if (newProduct == null)
+            {
+                return HttpNotFound();
+            }
+            return View(newProduct);
+        }
+
+        // POST: NewProducts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            NewProduct newProduct = db.NewProducts.Find(id);
+            db.NewProducts.Remove(newProduct);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BuildBakery/Utility.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BuildBakery
+{
+    public class Utility
+    {
+        public static void SlowSqlCall()
+        {
+            Random rnd = new Random();
+            int delay = rnd.Next(2, 7);
+
+            SqlConnection conn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["bakery"].ConnectionString);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand($"WAITFOR DELAY '00:00:0{delay}'", conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        public static void MakeDatabaseCallAndFetchData()
+        {
+            SqlConnection conn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["orders"].ConnectionString);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("Select * from NewProducts", conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        public static async Task CallWebApi()
+        {
+
+            Random rnd = new Random();
+            int delay = rnd.Next(5, 13); // creates a number between 1 and 12
+
+            HttpClient Client = new HttpClient();
+
+            Client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.162 Safari/537.36");
+            Client.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
+
+            var clientcontacts = Client.GetStringAsync("https://httpbin.org/delay/" + delay);
+
+            await Task.WhenAll(clientcontacts);
+        }
+
+        internal static void ThrowException()
+        {
+            throw new ApplicationException("Cannot let you query this record");
+        }
+
+        public static void MakeOutboundConnections()
+        {
+            (new WebClient()).OpenRead("http://www.bing.com/");
+        }
+    }
+}
